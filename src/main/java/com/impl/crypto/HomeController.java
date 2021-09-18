fe7be0947964a4e081dda0b1ef9dfc98f781286f
Ehsan Salmani <isanuric@protonmail.com>
2021-09-18 17:53:35 +0200

    impl controller

--- src/main/java/com/impl/crypto/HomeController.java
+++ src/main/java/com/impl/crypto/HomeController.java
@@ -9,58 +9,61 @@ import org.springframework.web.bind.annotation.PostMapping;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
+import java.nio.charset.StandardCharsets;
 import java.text.DateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 
 import static java.text.DateFormat.getDateTimeInstance;
-import static java.util.Arrays.*;
+import static java.util.Base64.getDecoder;
+import static java.util.Base64.getEncoder;
 import static java.util.Locale.getDefault;
 
 @Controller
 public class HomeController {
 
-    private Encryptor encryptor;
+    private final Encryptor encryptor;
 
     public HomeController(Encryptor encryptor) {
         this.encryptor = encryptor;
     }
 
-    @GetMapping("/time")
+    @GetMapping("/crypto")
     String getTime(Model model) {
-        // var date = getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, getDefault()).format(new Date());
-        // model.addAttribute("time", date);
-        //
-        // model.addAttribute("cars", new ArrayList<>(asList("Encrypt", "Decrypt")));
-        //
-        // try {
-        //     var encrypted = encryptor.doCrypto(Cipher.ENCRYPT_MODE, "abcd".getBytes());
-        //     model.addAttribute("encrypted", new String(encrypted));
-        //     var decrypted = encryptor.doCrypto(Cipher.DECRYPT_MODE, encrypted);
-        //     model.addAttribute("decrypted", new String(decrypted));
-        //
-        // } catch (CryptoException | IllegalBlockSizeException | BadPaddingException e) {
-        //     e.printStackTrace();
-        // }
-
+        setDate(model);
         model.addAttribute("formData", new FormData());
         return "index";
     }
 
-    @PostMapping("/time")
-    public String encrypt(@ModelAttribute("formData") FormData formData, Model model) {
-
-        model.addAttribute("cars", new ArrayList<>(asList("Encrypt", "Decrypt")));
-        System.out.println(formData.getMode());
-
+    @PostMapping("/crypto")
+    public String doCrypto(@ModelAttribute("formData") FormData formData, Model model) {
+        setDate(model);
         try {
-            var encrypted = encryptor.doCrypto(formData.getMode(), formData.getPlainText().getBytes());
-            model.addAttribute("encrypted", new String(encrypted));
+            var mode = formData.getMode();
+            var text = formData.getText();
+            var result = "";
+            if (mode == Cipher.ENCRYPT_MODE) {
+                var cryptoOutput = encryptor.doCrypto(mode, text.getBytes(StandardCharsets.UTF_8));
+                result = getEncoder().encodeToString(cryptoOutput);
+
+            } else if (mode == Cipher.DECRYPT_MODE) {
+                var cryptoOutput = encryptor.doCrypto(mode, getDecoder().decode(text));
+                result = new String(cryptoOutput);
+            }
+            model.addAttribute("cryptoResult", result);
+
         } catch (CryptoException | IllegalBlockSizeException | BadPaddingException e) {
             e.printStackTrace();
         }
-
         return "index";
     }
+
+    private void setDate(Model model) {
+        var date = getDateTimeInstance(
+                DateFormat.LONG,
+                DateFormat.LONG,
+                getDefault())
+                .format(new Date());
+        model.addAttribute("time", date);
+    }
 }
+
