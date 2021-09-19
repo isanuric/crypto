6f604cd1811596f6cba5bdf21c445778806c38fa
Ehsan Salmani <isanuric@protonmail.com>
2021-09-19 21:03:07 +0200

    impl controller crypto-file

--- src/main/java/com/impl/crypto/HomeController.java
+++ src/main/java/com/impl/crypto/HomeController.java
@@ -1,15 +1,25 @@
 package com.impl.crypto;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
+import java.io.File;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.text.DateFormat;
 import java.util.Date;
 
@@ -17,9 +27,15 @@ import static java.text.DateFormat.getDateTimeInstance;
 import static java.util.Base64.getDecoder;
 import static java.util.Base64.getEncoder;
 import static java.util.Locale.getDefault;
+import static org.apache.commons.lang3.StringUtils.isEmpty;
 
 @Controller
 public class HomeController {
+
+    public static final String MESSAGE_ATTR = "message";
+    public static final String INDEX_HTML = "index";
+    @Value("${uploads.path}")
+    private String uploadsPath;
 
     private final Encryptor encryptor;
 
@@ -28,10 +44,10 @@ public class HomeController {
     }
 
     @GetMapping("/crypto")
-    String getTime(Model model) {
+    public String getTime(Model model) {
         setDate(model);
         model.addAttribute("formData", new FormData());
-        return "index";
+        return INDEX_HTML;
     }
 
     @PostMapping("/crypto")
@@ -54,7 +70,7 @@ public class HomeController {
         } catch (CryptoException | IllegalBlockSizeException | BadPaddingException e) {
             e.printStackTrace();
         }
-        return "index";
+        return INDEX_HTML;
     }
 
     private void setDate(Model model) {
@@ -65,5 +81,39 @@ public class HomeController {
                 .format(new Date());
         model.addAttribute("time", date);
     }
+
+    @PostMapping("/crypto-file")
+    public String uploadFile(
+            @RequestParam("file") MultipartFile multipartFile,
+            @ModelAttribute("formData") FormData formData, Model model) {
+        model.addAttribute("formData", new FormData());
+
+        var redirectUrl = "redirect:/crypto";
+
+        if (multipartFile.isEmpty() || isEmpty(multipartFile.getOriginalFilename())) {
+            model.addAttribute(MESSAGE_ATTR, "Please select a file to upload.");
+            return INDEX_HTML;
+        }
+
+        var fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+        try {
+            Path path = Paths.get(uploadsPath + fileName);
+            Files.copy(multipartFile.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+            String fileNameResult = formData.getMode() == 1 ? "encrypted.txt" : "decrypted.txt";
+            encryptor.doCryptoFile(
+                    formData.getMode(),
+                    new File(uploadsPath + fileName),
+                    new File(uploadsPath + fileNameResult));
+
+        } catch (IOException | CryptoException | IllegalBlockSizeException | BadPaddingException e) {
+            e.printStackTrace();
+            model.addAttribute(MESSAGE_ATTR, "Can not execute cryptography process: " + e.getMessage());
+            return redirectUrl;
+        }
+
+        model.addAttribute(MESSAGE_ATTR, "Cryptography process successfully done for: " + fileName);
+        return redirectUrl;
+    }
 }
+
 
