d79d4129b6d54cefaffe80580de546a879cdea02
Ehsan Salmani <isanuric@protonmail.com>
2021-10-12 22:09:52 +0200

    implement RegisterForm.js

--- cryptofrontend/src/components/Appbar.js
+++ cryptofrontend/src/components/Appbar.js
@@ -9,7 +9,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar style={{ background: '#2E3B55' }}>
         <Toolbar>
           <IconButton
             size="large"
--- cryptofrontend/src/components/RegisterForm.js
+++ cryptofrontend/src/components/RegisterForm.js
@@ -11,10 +11,27 @@ export class RegisterForm extends React.Component {
     
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChangeWeekDay = this.handleChangeWeekDay.bind(this);
       }
     
       handleChange(event) {
-        this.setState({value: event.target.value});
+          // event.target zeigt auf html element (hier text box)
+          // event.target.value zeigt auf value von html element (hier value in text box)
+          console.log(event.target.value);
+          
+          // value is this.state.value. See lines 10 and 38
+          this.setState({
+                value: event.target.value,
+                weekDay: 'Monday'
+           });
+      }
+
+      handleChangeWeekDay(event) {
+          console.log(event.target);
+
+          this.setState ({
+            weekday: event.target.value
+          })
       }
     
       handleSubmit(event) {
@@ -32,6 +49,18 @@ export class RegisterForm extends React.Component {
                 </label>
                 <input type="submit" value="Submit" />
             </form>
+
+            <br></br>
+            <select 
+            value={this.state.weekday} onChange={this.handleChangeWeekDay}>
+                <option value='AA'>1</option>
+                <option value="BB">2</option>
+                <option value='CC'>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+            </select>
           </Paper>
 
         );
